5544999206e4a60259f736f2fe79f0346a812281
ArkChoi <ark.goya@gmail.com>
2025-09-22 09:13:13 +0900

    SumMultipleThreeAdd

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 int main()
 {
 	int sum = 0;
+	int SumMultipleofThree = 0;
+
 	for (int i=1;i<101;i++) 
 	{
 		sum += i;
+		if (i%3==0) 
+		{
+			SumMultipleofThree += i;
+		}
 	}
-	cout << sum << endl;
+
+	cout<< "1~100 의 합 : " << sum << endl;
+	cout << "1~100 의 3의 배수 합 : " << SumMultipleofThree << endl;
 }
