fbeecc6b3d5a1ad54a4d29d32dc107277d112570
ArkChoi <ark.goya@gmail.com>
2025-09-22 15:01:07 +0900

    LowLowLowLevelGame

---
+++ Source.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int Source() 
+{
+	//1번 실습
+	/*int Sum = 0;
+	int SumMultipleofThree = 0;
+
+	for (int i=1;i<101;i++)
+	{
+		Sum += i;
+		if (i%3==0)
+		{
+			SumMultipleofThree += i;
+		}
+	}
+
+	cout<< "1~100 의 합 : " << Sum << endl;
+	cout << "1~100 의 3의 배수 합 : " << SumMultipleofThree << endl;*/
+
+	//2번 실습
+	/*int MaxValue = 0;
+	int CountLoop = 0;
+	cin >> MaxValue;
+
+	for (int i=0;i<MaxValue;i++)
+	{
+		for (int j=0;j<i;j++)
+		{
+			cout << " ";
+		}
+		for (int CountValue = MaxValue ; CountValue > CountLoop; CountValue--)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+		CountLoop++;
+	}*/
+	//3번 실습
+	//int MaxSize = 10;
+
+	////LoadLevel
+	//int World[10][10] = {
+	//	{1,1,1,1,1,1,1,1,1,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,1,1,1,1,1,1,1,1,1},
+	//};
+
+	//int PlayerX = 1;
+	//int PlayerY = 1;
+	//char PlayerShape = 'P';
+
+	////사상(Mapping)
+	//char Sprites[10] = { ' ', '*', };
+
+	//bool bIsRunning = true;
+
+	//while (bIsRunning)
+	//{
+	//	//Input
+	//	int KeyCode = _getch();
+
+	//	//Process
+	//	switch (KeyCode)
+	//	{
+	//	case 'w':
+	//		PlayerY--;
+	//		break;
+	//	case 'a':
+	//		PlayerX--;
+	//		break;
+	//	case 's':
+	//		PlayerY++;
+	//		break;
+	//	case 'd':
+	//		PlayerX++;
+	//		break;
+	//	case 'q':
+	//		bIsRunning = false;
+	//		break;
+
+	//	default:
+	//		break;
+	//	}
+
+	//	//Render
+	//	system("cls"); //clear scline?
+	//	//페어링 이슈.. double buffer?
+	//	for (int Y = 0; Y < MaxSize; Y++)
+	//	{
+	//		for (int X = 0; X < MaxSize; X++)
+	//		{
+	//			if (X == PlayerX && Y == PlayerY)
+	//			{
+	//				cout << PlayerShape;
+	//			}
+	//			else if (World[X][Y] == 1)
+	//			{
+	//				cout << Sprites[1];
+	//			}
+	//			else
+	//			{
+	//				cout << Sprites[0];
+	//			}
+	//			cout << ' ';
+	//		}
+	//		cout << "\n";
+	//	}
+	//}
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,44 +1,114 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+//scope (영역) => {이 안쪽을 의미}
+int Input();
+void Process(int KeyCode);
+void Render();
+
+//Globla Value
+int MaxSize = 10;
+
+//LoadLevel
+int World[10][10] = {
+	{1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1},
+};
+
+//사상(Mapping)
+char Sprites[10] = { ' ', '*', };
+
+//Character
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
+
+//Playing
+bool bIsRunning = true;
+
+//Globla Value End
+
 int main()
 {
-	//1번 실습
-	/*int Sum = 0;
-	int SumMultipleofThree = 0;
-
-	for (int i=1;i<101;i++) 
+	//3번 실습
+	//frame, deltaseconds
+	while (bIsRunning)
 	{
-		Sum += i;
-		if (i%3==0) 
-		{
-			SumMultipleofThree += i;
-		}
+		//Input
+		int KeyCode = Input();
+		//Process, Tick
+		Process(KeyCode);
+
+		//Render
+		Render();
 	}
+	return 0;
+}
 
-	cout<< "1~100 의 합 : " << Sum << endl;
-	cout << "1~100 의 3의 배수 합 : " << SumMultipleofThree << endl;*/
+int Input() 
+{
+	int KeyCode = _getch();
+	return KeyCode;
+}
 
-	//2번 실습
-	int MaxValue = 0;
-	int CountLoop = 0;
-	cin >> MaxValue;
+//(int input, int Location X, int Location Y)
+void Process(int KeyCode) 
+{
+	switch (KeyCode)
+	{
+	case 'w':
+		PlayerY--;
+		break;
+	case 'a':
+		PlayerX--;
+		break;
+	case 's':
+		PlayerY++;
+		break;
+	case 'd':
+		PlayerX++;
+		break;
+	case 'q':
+		bIsRunning = false;
+		break;
 
-	for (int i=0;i<MaxValue;i++) 
-	{	
-		for (int j=0;j<i;j++)
+	default:
+		break;
+	}
+}
+
+void Render() 
+{
+	system("cls"); //clear scline?
+	//페어링 이슈.. double buffer?
+	for (int Y = 0; Y < MaxSize; Y++)
+	{
+		for (int X = 0; X < MaxSize; X++)
 		{
-			cout << " ";
-		}
-		for (int CountValue = MaxValue ; CountValue > CountLoop; CountValue--)
-		{
-			cout << "*";
+			if (X == PlayerX && Y == PlayerY)
+			{
+				cout << PlayerShape;
+			}
+			else if (World[X][Y] == 1)
+			{
+				cout << Sprites[1];
+			}
+			else
+			{
+				cout << Sprites[0];
+			}
+			cout << ' ';
 		}
 		cout << "\n";
-		CountLoop++;
 	}
-
-
-	return 0;
 }
