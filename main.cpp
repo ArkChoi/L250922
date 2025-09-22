516fb7a3e566cf408ddfce1f55b7ee1face5327a
ArkChoi <ark.goya@gmail.com>
2025-09-22 16:31:19 +0900

    StringStudy

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	
+	//char Chars[6] = {'H','e','l','l','o','\0'};
+	char Chars[6] = "Hello";
+
+	cout << Chars << endl;
+
+	string String = "Hello";
+	cout << String << endl;
+
 	return 0;
 }
